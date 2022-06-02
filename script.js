08bd292a81f4bb0becd32dca67f1f88e3080c50e
Jason Gutierrez <jasongeee@yahoo.com>
2022-06-02 12:36:09 -0700

    Added delete function to remove cards

--- script.js
+++ script.js
@@ -6,59 +6,129 @@ function Book(Title, Author, Pages, Read) {
     this.Author = Author;
     this.Pages = Pages;
     this.Read = Read;
-    // this.function = function() {
-    //     return (title + author + ", " + pages)
-    // }
-
 }
 
 // Function to add into array
 function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
     myLibrary.push(book);
+    setData(); // saves data to local storage
+    restore();
+    displayBooks();
 }
 
 // Function to display books
 function displayBooks() {
     const books = document.querySelector(".books");
 
+    // to remove duplicates of added books
+    const removeDiv = document.querySelectorAll('.card');
+    for (let i = 0; i < removeDiv.length; i++) {
+        removeDiv[i].remove();
+    }
+
     // loop into the library array to add cards
-    myLibrary.forEach(myLibrary => {
+    let index = 0;
+    myLibrary.forEach(myLibrarys => {
+
+        // creates div and adds card class
         const card = document.createElement("div");
         card.classList.add("card");
         books.appendChild(card);
 
-        for (let key in myLibrary) {
-            console.log(`${key}: ${myLibrary[key]}`);
+        // create button to remove book
+        const removeBookBtn = document.createElement('button');
+        removeBookBtn.classList.add('remove-book');
+        removeBookBtn.textContent = "Remove Book";
+
+        // links data attribute of array to card to remove
+        removeBookBtn.dataset.linkedArray = index;
+        index++;
+        card.appendChild(removeBookBtn);
+
+        // removes the book from library
+        removeBookBtn.addEventListener('click', () => {
+            let rm = removeBookBtn.dataset.linkedArray;
+            myLibrary.splice(parseInt(rm), 1);
+            card.remove();
+            setData();
+            restore();
+            displayBooks();
+        });
+
+        // loops through object to display onto card
+        for (let key in myLibrarys) {
+            console.log(`${key}: ${myLibrarys[key]}`);
             const para = document.createElement("p");
-            para.textContent = (`${key}: ${myLibrary[key]}`);
+            para.textContent = (`${key}: ${myLibrarys[key]}`);
             card.appendChild(para);
         }
-        
     });
 }
 
-// Function to blur background as we add new book
+// Function to blur background and display the book form
 function toggleBlur() {
-    const blur = document.querySelector(".add-book-btn");
-    const active = document.getElementById("blur");
+    const blur = document.querySelector('.add-book-btn'); //button
+    const active = document.getElementById('blur'); //blur ID to active
+    const bookForm = document.getElementById('book-form'); //
     blur.addEventListener('click', () => {
-        active.classList.toggle('active');
-        // bookButton();
+        active.classList.toggle('active'); // add 'active' class
+        bookForm.classList.toggle('active'); // add 'active' class
     });
 }
 
-// Function to add user input for books and display the form
-function bookButton() {
+// Function to submit data to add book to library
+function formData() {
+    let Title = document.getElementById('Title').value;
+    let Author = document.getElementById('Author').value;
+    let Pages = document.getElementById('Pages').value;
+    let Read = document.getElementById('Read').value;
 
+    //if empty, do not allow
+    if ((Title == "") || (Author == "") || (Pages == "") || (Read == "")) {
+        return alert("Must fill out whole form!");
+    }
+
+    addBookToLibrary(Title, Author, Pages, Read);
+
+    document.getElementById('add-book').reset();
 }
 
-addBookToLibrary("Blah Blah", "John Doe", 1234, "not read");
-addBookToLibrary("Blah Blah", "John Doe", 1234, "not read");
-addBookToLibrary("Blah Blah", "John Doe", 1234, "not read");
-addBookToLibrary("Blah Blah", "John Doe", 1234, "not read");
-addBookToLibrary("Blah Blah", "John Doe", 1234, "not read");
-addBookToLibrary("Blah Blah", "John Doe", 1234, "not read");
+// Listens for submit click to add to library
+const submitBtn = document.querySelector('.submit');
+const removeBlur = document.getElementById('blur'); // remove blur effect
+const removeForm = document.getElementById('book-form'); // remove form after submitting
+submitBtn.addEventListener('click', formData);
+submitBtn.addEventListener('click', () => {
+    formData;
+    removeBlur.classList.remove('active');
+    removeForm.classList.remove('active');
+});
 
-toggleBlur();
-displayBooks();
+// Listens for reset click
+const resetForm = document.querySelector('.reset');
+resetForm.addEventListener('click', () => {
+    document.getElementById('add-book').reset();
+});
+
+// Saves data to local storage
+function setData() {
+    localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
+}
+
+// brings back local storage when page refreshed
+function restore() {
+    if (!localStorage.myLibrary) {
+        displayBooks();
+    } else {
+        let objects = localStorage.getItem('myLibrary') // gets information from local storage to use in below loop to create DOM/display
+        objects = JSON.parse(objects);
+        myLibrary = objects;
+        displayBooks();
+    }
+}
+
+toggleBlur(); // Toggles blur and displays form
+displayBooks(); // Displays our library
+restore();
+
